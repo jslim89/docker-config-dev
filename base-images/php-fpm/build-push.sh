e18a4ef04ec574be407b77a79af84a72f3fd2cd1
Js Lim <jslim89@gmail.com>
2021-01-31 16:32:14 +0800

    added tag latest command

--- base-images/php-fpm/build-push.sh
+++ base-images/php-fpm/build-push.sh
@@ -4,3 +4,5 @@ export TAG=$1
 
 docker-compose -f build.yml build --no-cache php-fpm-base
 docker push jslim/php8-laravel-base:${TAG}
+docker image tag jslim/php8-laravel-base:${TAG} jslim/php8-laravel-base:latest
+docker image push jslim/php8-laravel-base:latest
