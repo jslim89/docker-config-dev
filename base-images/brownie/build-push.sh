e682804d2f9fad01ebce23d7b35ca3243cfd34ea
Js Lim <jslim89@gmail.com>
2022-03-13 08:52:17 +0800

    added python brownie base image

---
+++ base-images/brownie/build-push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export TAG=$1
+export PYTHON_VERSION=$2
+
+docker-compose -f build.yml build --no-cache python-brownie
+docker push jslim/python-brownie:${TAG}
+docker image tag jslim/python-brownie:${TAG} jslim/python-brownie:latest
+docker image push jslim/python-brownie:latest
