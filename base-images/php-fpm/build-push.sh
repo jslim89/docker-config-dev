9df8c73d7158cb59b0fe891506d4864c3a4cf991
Js Lim <jslim89@gmail.com>
2022-06-26 14:16:50 +0800

    refactor php fpm base image build

--- base-images/php-fpm/build-push.sh
+++ base-images/php-fpm/build-push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 export TAG=$1
-export PHP_VERSION=$2
 
-docker-compose -f build.yml build --no-cache php-fpm-base
-docker push jslim/php${PHP_VERSION}-laravel-base:${TAG}
-docker image tag jslim/php${PHP_VERSION}-laravel-base:${TAG} jslim/php${PHP_VERSION}-laravel-base:latest
-docker image push jslim/php${PHP_VERSION}-laravel-base:latest
+docker-compose -f build.yml build --no-cache php-fpm-laravel
+docker push jslim/php-fpm-laravel:${TAG}
+docker image tag jslim/php-fpm-laravel:${TAG} jslim/php-fpm-laravel:latest
+docker image push jslim/php-fpm-laravel:latest
